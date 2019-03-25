e3d62d2d629d2485ab90d2839471899f95a7a6a4
Andy <amonroe805@protonmail.com>
2019-03-24 22:56:48 -0500

    Start implementing word predictions

--- HMM.hs
+++ HMM.hs
@@ -1,6 +1,11 @@
 import Data.Char
 import Data.Map (Map)
 import qualified Data.Map as Map
+import System.Random
+
+seed::Int
+seed = 40
+generator = mkStdGen seed
 
 tokenize input = words (stringToLower (noPunc input))
   where
@@ -14,9 +19,27 @@ learn (k1 : k2 : v : tokens)
 
 
 initialMap :: Map (Maybe String, Maybe String) [String]
-initialMap = Map.fromList [((Nothing, Nothing), [])]
+initialMap = Map.fromList [((Nothing, Nothing), [""])]
 
 addToken :: (Maybe String, Maybe String) -> String -> Map (Maybe String, Maybe String) [String] -> Map (Maybe String, Maybe String) [String]
 addToken k v m =  Map.insertWith (++) k [v] (
                     Map.insertWith (++) ((fst k), Nothing) [v] (
                         Map.insertWith (++) (Nothing, snd k) [v] m))
+
+predictWord :: (Maybe String, Maybe String) -> Map (Maybe String, Maybe String) [String] -> String
+predictWord k m =
+  case Map.lookup k m of
+    Just x -> getRandomElement x
+    Nothing -> case Map.lookup (Nothing, snd k) m of
+      Just x -> getRandomElement x
+      Nothing -> case Map.lookup (fst k, Nothing) m of
+        Just x -> getRandomElement x
+        Nothing -> case Map.lookup (Nothing, Nothing) m of
+          Just x -> getRandomElement x
+          Nothing -> ""
+
+getRandomElement :: [String] -> String
+getRandomElement l = l !! rand where
+  n = length l
+  (rand, _) = randomR (0,(n-1)) generator
+
