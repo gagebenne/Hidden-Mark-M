ab2ac1c50c563490b9093a46659a6893948d6658
Gage Benne <gagedbenne@gmail.com>
2019-03-25 15:31:54 -0500

    Tweet, tweet!

--- HMM.hs
+++ HMM.hs
@@ -17,28 +17,30 @@ type Tweet = [Text]
 
 main = do
   s <- readFile "trump_tweets/2018.tweets"
-  print (learnTweets (tokenize (Text.pack s)))
-  -- Text.putStrLn (Text.pack (predictTweet (Text.pack "MEXICO") (Text.pack "MEXICO") s))
+  Text.putStrLn (predictTweet (Text.pack "MAKE") (Text.pack "AMERICA") (learn (Text.pack s)))
   return 1
+
+learn :: Text -> HMM
+learn input = Map.union (learnTweets (tokenize input)) (learnFirsts (tokenize input))
 
 tokenize :: Text -> [Tweet]
 tokenize input = map Text.words (Text.lines (Text.toUpper input))
 
+learnFirsts :: [Tweet] -> HMM
+learnFirsts (tweet : tweets) = Map.unionWith (++) (learnFirst tweet) (learnFirsts tweets)
+learnFirsts [] = Map.empty
+
+learnFirst :: Tweet -> HMM
+learnFirst tweet = Map.fromList [((Nothing, Just (Text.pack "\n")), [tweet!!0]), ((Nothing, Nothing), [tweet!!0])]
+
 learnTweets :: [Tweet] -> HMM
-learnTweets (tweet : tweets)
-  | tweets == [] = learnTweet tweet
-  | otherwise = Map.union (learnTweet tweet) (learnTweets tweets)
+learnTweets (tweet : tweets) = Map.unionWith (++) (learnTweet tweet) (learnTweets tweets)
+learnTweets [] = Map.empty
 
 learnTweet :: Tweet -> HMM
-learnTweet (t1 : t2 : t3 : tokens)
-  | tokens == [] = addToken (Just t1, Just t2) t3 initialHMM
-  | otherwise = addToken (Just t1, Just t2) t3 (learnTweet ([t2] ++ [t3] ++ tokens) )
-learnTweet [t1, t2] = error "TWO"
-learnTweet [t1] = error "ONE"
-
--- create initial state of HMM with (Nothing, Nothing) -> [""] key-value pair
-initialHMM :: HMM
-initialHMM = Map.fromList [((Nothing, Nothing), [""])]
+learnTweet (t1 : t2 : t3 : tokens) = addToken (Just t1, Just t2) t3 (learnTweet ([t2] ++ [t3] ++ tokens))
+learnTweet [t1, t2] = addToken (Just t1, Just t2) (Text.pack "\n") (learnTweet [])
+learnTweet _ = Map.empty
 
 -- given a key, a token, and an HMM, produce an updated HMM
 addToken :: Key -> Token -> HMM -> HMM
@@ -46,19 +48,12 @@ addToken k v m =  Map.insertWith (++) k [v] (
                     Map.insertWith (++) ((fst k), Nothing) [v] (
                         Map.insertWith (++) (Nothing, snd k) [v] m))
 
--- given a list of tokens, produce an HMM
-learn :: [Token] -> HMM
-learn (k1 : k2 : v : tokens)
-  | tokens == [] = addToken (Just k1, Just k2) v initialHMM
-  | k1 == "~" = addToken (Nothing, Nothing) k2 (learn (k2 : v : tokens) )
-  | otherwise = addToken (Just k1, Just k2) v (learn (k2 : v : tokens) )
-
 -- select a random element from a list
 getRandomElement :: [a] -> a
 getRandomElement l = l !! (unsafePerformIO (getStdRandom (randomR (0, (length l) - 1))))
 
 -- predict a token given a key and an HMM
-predictWord :: Key -> HMM -> Token 
+predictWord :: Key -> HMM -> Token
 predictWord k m =
   case Map.lookup k m of
     Just x -> getRandomElement x
@@ -71,17 +66,16 @@ predictWord k m =
           Nothing -> Text.pack ""
 
 -- produces an endless list of recursively predicted tokens based on two starter tokens
-predictForever :: Token -> Token -> HMM -> [Token]
-predictForever w1 w2 m = map fst output
-    where
-        output = [(w1, w2)] ++ [ ( snd w, predictWord ( Just (fst w),  Just (snd w)) m) | w <- output]
+predictionStream :: Token -> Token -> HMM -> [Token]
+predictionStream w1 w2 hmm = map fst stream
+  where
+    stream = [(w1, w2)] ++ [ ( snd w, predictWord ( Just (fst w),  Just (snd w)) hmm) | w <- stream]
 
 -- predict list of tokens until an end token is found
 --predictTweet :: Token -> Token -> HMM -> [Token]
 --predictTweet w1 w2 m = takeWhileInclusive notEndToken (predictForever w1 w2 m)
 
-predictTweet :: Text -> Text -> Text -> Text
-predictTweet w1 w2 input = Text.unwords (takeWhile ((/=) (Text.pack "~")) (map fst output))
+predictTweet :: Text -> Text -> HMM -> Text
+predictTweet w1 w2 hmm = Text.unwords (takeWhile ((/=) (Text.pack "\n")) stream)
   where
-    output = [(w1, w2)] ++ [ ( snd w, predictWord ( Just (fst w),  Just (snd w)) m) | w <- output]
-      where m = learnTweets (tokenize input)
+    stream = predictionStream w1 w2 hmm
