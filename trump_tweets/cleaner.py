119599395ba97ce4b198fa96778350b98d76739e
Andy <amonroe805@protonmail.com>
2019-03-24 21:35:37 -0500

    Improve data cleaning

--- trump_tweets/cleaner.py
+++ trump_tweets/cleaner.py
@@ -1,11 +1,18 @@
 import json
 import re
+import os
+import html
 
 years = list(range(2009,2019))
 
 for yr in years:
 
     ifn = 'condensed_' + str(yr) + '.json'
+    ofn = str(yr) + '.tweets'
+
+    if os.path.isfile(ofn):
+        os.remove(ofn)
+
     with open(ifn) as f:
         data = json.load(f)
         f.close()
@@ -14,11 +21,11 @@ for yr in years:
     
     rx = re.compile('http[^\s]*')
     
-    ofn = str(yr) + '.tweets'
     outfile = open(ofn,'a') 
     for elem in data:
         res = rx.sub(' ', elem['text']).strip()
-        outfile.write(res)
+        output = html.unescape(res)
+        outfile.write(output)
         outfile.write('\n')
     
     outfile.close()
