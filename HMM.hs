fb7b9273b556a2225dd5f341076ade16757ac275
Gage Benne <gagedbenne@gmail.com>
2019-03-25 13:46:19 -0500

    Many changes to get Data.Text going

--- HMM.hs
+++ HMM.hs
@@ -4,37 +4,49 @@ import qualified Data.Map as Map
 import System.Random
 import Data.List
 import System.IO.Unsafe
+import Data.Text hiding (map, takeWhile, length)
+import Data.Text (Text)
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
 
 
-predictTweet :: String -> String -> Map (Maybe String, Maybe String) [String] -> [String]
-predictTweet w1 w2 m = map fst output
-    where
-        output = [(w1, w2)] ++ [ ( snd w, predictWord ( Just (fst w),  Just (snd w)) m) | w <- output]
+main = do
+  s <- readFile "trump_tweets/2018.tweets"
+  print (learnTweets (tokenize (Text.pack s)))
+  -- Text.putStrLn (Text.pack (predictTweet (Text.pack "MEXICO") (Text.pack "MEXICO") s))
+  return 1
 
-tokenize :: String -> [String]
-tokenize input = words (format input)
-  where format :: String -> String
-        format s =  map toUpper (
-          intercalate " " (terminate input))
+predictTweet :: Text -> Text -> Text -> Text
+predictTweet w1 w2 input = Text.unwords (takeWhile ((/=) (Text.pack "~")) (map fst output))
+  where
+    output = [(w1, w2)] ++ [ ( snd w, predictWord ( Just (fst w),  Just (snd w)) m) | w <- output]
+      where m = learnTweets (tokenize input)
 
-terminate input = [ x ++ " ~" | x <- (lines input) ]
+tokenize :: Text -> [[Text]]
+tokenize input = map Text.words (Text.lines (Text.toUpper input))
 
-learn :: [String] -> Map (Maybe String, Maybe String) [String]
-learn (k1 : k2 : v : tokens)
-  | tokens == [] = addToken (Just k1, Just k2) v initialMap
-  | k1 == "~" = addToken (Nothing, Nothing) k2 (learn (k2 : v : tokens) )
-  | otherwise = addToken (Just k1, Just k2) v (learn (k2 : v : tokens) )
+learnTweets :: [[Text]] -> Map (Maybe Text, Maybe Text) [Text]
+learnTweets (tweet : tweets)
+  | tweets == [] = learnTweet tweet
+  | otherwise = Map.union (learnTweet tweet) (learnTweets tweets)
+
+learnTweet :: [Text] -> Map (Maybe Text, Maybe Text) [Text]
+learnTweet (t1 : t2 : t3 : tokens)
+  | tokens == [] = addToken (Just t1, Just t2) t3 initialMap
+  | otherwise = addToken (Just t1, Just t2) t3 (learnTweet ([t2] ++ [t3] ++ tokens) )
+learnTweet [t1, t2] = error "TWO"
+learnTweet [t1] = error "ONE"
 
 
-initialMap :: Map (Maybe String, Maybe String) [String]
-initialMap = Map.fromList [((Nothing, Nothing), [""])]
+initialMap :: Map (Maybe Text, Maybe Text) [Text]
+initialMap = Map.fromList [((Nothing, Nothing), [Text.pack ""])]
 
-addToken :: (Maybe String, Maybe String) -> String -> Map (Maybe String, Maybe String) [String] -> Map (Maybe String, Maybe String) [String]
+addToken :: (Maybe Text, Maybe Text) -> Text -> Map (Maybe Text, Maybe Text) [Text] -> Map (Maybe Text, Maybe Text) [Text]
 addToken k v m =  Map.insertWith (++) k [v] (
                     Map.insertWith (++) ((fst k), Nothing) [v] (
                         Map.insertWith (++) (Nothing, snd k) [v] m))
 
-predictWord :: (Maybe String, Maybe String) -> Map (Maybe String, Maybe String) [String] -> String
+predictWord :: (Maybe Text, Maybe Text) -> Map (Maybe Text, Maybe Text) [Text] -> Text
 predictWord k m =
   case Map.lookup k m of
     Just x -> getRandomElement x
@@ -44,7 +56,7 @@ predictWord k m =
         Just x -> getRandomElement x
         Nothing -> case Map.lookup (Nothing, Nothing) m of
           Just x -> getRandomElement x
-          Nothing -> ""
+          Nothing -> Text.pack ""
 
-getRandomElement :: [String] -> String
+getRandomElement :: [Text] -> Text
 getRandomElement l = l !! (unsafePerformIO (getStdRandom (randomR (0, (length l) - 1))))
